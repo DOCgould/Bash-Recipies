27026902721dfa8391c65b1c2280e406ab429e8b
Christian Gould <christian.d.gould@gmail.com>
2022-05-29 18:29:34 -0700

    Update generate-project.sh
    Should be able to build executable with project name ... ( default config )

--- generate-project.sh
+++ generate-project.sh
@@ -16,36 +16,36 @@
 
 function gen_cmake()
 {
-echo "#" > CMakeLists.txt
-echo "# Generic CmakeList file" >> CMakeLists.txt
-echo "#" >> CMakeLists.txt
-echo "# @author Christian Gould" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "cmake_minimum_required(VERSION 3.22)" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
+echo '#' > CMakeLists.txt
+echo '# Generic CmakeList file' >> CMakeLists.txt
+echo '#' >> CMakeLists.txt
+echo '# @author Christian Gould' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo 'cmake_minimum_required(VERSION 3.22)' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
 echo "project(${PROJECT_NAME})" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "# includes" >> CMakeLists.txt
-echo "include_directories(" >> CMakeLists.txt
-echo "  include" >> CMakeLists.txt
-echo ")" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "# find header & source" >> CMakeLists.txt
-echo "file(GLOB_RECURSE HEADER "include/*.h")" >> CMakeLists.txt
-echo "file(GLOB_RECURSE SOURCE "src/*.cpp")" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo '# includes' >> CMakeLists.txt
+echo 'include_directories(' >> CMakeLists.txt
+echo '  include' >> CMakeLists.txt
+echo ')' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo '# find header & source' >> CMakeLists.txt
+echo 'file(GLOB_RECURSE HEADER 'include/*.h')' >> CMakeLists.txt
+echo 'file(GLOB_RECURSE SOURCE 'src/*.cpp')' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
 echo "add_executable(${PROJECT_NAME}" >> CMakeLists.txt
-echo "  ${SOURCE}" >> CMakeLists.txt
-echo "  ${HEADER}" >> CMakeLists.txt
-echo ")" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "source_group("Header include" FILES ${HEADER})" >> CMakeLists.txt
-echo "source_group("Source src"     FILES ${SOURCE})" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
-echo "# link libraries" >> CMakeLists.txt
-echo "#target_link_libraries(${PROJECT_NAME} ${SOME_LIB})" >> CMakeLists.txt
-echo "" >> CMakeLists.txt
+echo '  ${SOURCE}' >> CMakeLists.txt
+echo '  ${HEADER}' >> CMakeLists.txt
+echo ')' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo 'source_group('Header include' FILES ${HEADER})' >> CMakeLists.txt
+echo 'source_group('Source src'     FILES ${SOURCE})' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
+echo '# link libraries' >> CMakeLists.txt
+echo '#target_link_libraries(${PROJECT_NAME} ${SOME_LIB})' >> CMakeLists.txt
+echo '' >> CMakeLists.txt
 }
 
 function gen_proj_files()
